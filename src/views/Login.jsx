3be7cabe28aee3497a01c2822c3a25c81692e3e7
Francisco <gamercrepy8@gmail.com>
2024-11-24 19:41:24 -0600

    actualizacion de estilos

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./../styles/Sesion.css";
-import "./../styles/General.css";
 import { InputDefault, InputPassword } from "./../components/Inputs";
 
 function Login() {
